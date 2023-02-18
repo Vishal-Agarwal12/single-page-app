2b9df7dc771a8f98392f51d939f99e53b20f0097
Vishal Agarwal <vishalagarwal5720@gmail.com>
2023-02-18 11:16:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 import {
   BrowserRouter as Router,
@@ -6,23 +6,22 @@ import {
   Route,
 } from "react-router-dom";
 
-import Header from './components/Header';
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Router>
-
         <Header />
         <Routes>
-
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
-
+          
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          
         </Routes>
       </Router>
     </div>
