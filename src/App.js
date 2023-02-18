abe6cd8459992a236879437ad43d8a362571efa8
Vishal Agarwal <vishalagarwal5720@gmail.com>
2023-02-18 18:56:37 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
+  Route, 
 } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 function App() {
   return (
     <div className="App">
       <Router>
+
         <Header />
         <Routes>
-          
-            <Route path="*" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
-          
+
+            <Route path="/" element={<Home />} exact />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+         
         </Routes>
         
       </Router>
